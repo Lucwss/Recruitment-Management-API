42cf9dc6e0b01d45d1a601cbb3b62a77011630d3
lucas <lucws2002@gmail.com>
2025-07-27 23:11:11 -0400

    feat: Add repository for Vacancy

---
+++ domain/usecases/create_vacancy.py
@@ -0,0 +1,36 @@
+import json
+import traceback
+
+from application.dto.vacancy import VacancyInput, VacancyOutput
+from web.http_response_schema import HttpResponse
+
+from application.interfaces.usecase import UseCase
+from domain.interfaces.vacancy_repository import IVacancyRepository
+from web.http_response_schema import HttpResponseSchema
+
+
+class CreateVacancyUseCase(UseCase):
+
+    """
+    Use case for creating a vacancy in the system (Implementing the UseCase interface).
+    """
+
+    def __init__(self, repository: IVacancyRepository):
+        """
+        Initialize the CreateVacancyUseCase with a repository.
+
+        :param repository: An instance of IVacancyRepository to interact with vacancy data.
+        """
+
+        self.repository = repository
+
+
+    async def execute(self, vacancy_data_input: VacancyInput) -> HttpResponse:
+
+        try:
+            created_vacancy: VacancyOutput = await self.repository.create_vacancy(vacancy_data_input)
+            created_vacancy_dict = json.loads(created_vacancy.model_dump_json())
+            return HttpResponseSchema.created(created_vacancy_dict)
+        except Exception as e:
+            traceback.print_exc()
+            return HttpResponseSchema.internal_server_error(Exception(e))
