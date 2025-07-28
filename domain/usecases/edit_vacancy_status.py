4dde076617bcec0e2e201b825308540c70cd7e56
lucas <lucws2002@gmail.com>
2025-07-28 17:11:41 -0400

    test: Add tests to validate vacancy status

---
+++ domain/usecases/edit_vacancy_status.py
@@ -0,0 +1,46 @@
+import json
+import traceback
+
+from application.dto.vacancy import VacancyOutput, StatusToUpdate
+from web.http_response_schema import HttpResponse
+
+from application.interfaces.usecase import UseCase
+from domain.interfaces.vacancy_repository import IVacancyRepository
+from web.http_response_schema import HttpResponseSchema
+
+
+class EditVacancyStatusUseCase(UseCase):
+
+    """
+    Use case for editing a vacancy status by its id in the system (Implementing the UseCase interface).
+    """
+
+    def __init__(self, repository: IVacancyRepository):
+        """
+        Initialize the EditVacancyStatusUseCase with a repository.
+
+        :param repository: An instance of IVacancyRepository to interact with vacancy data.
+        """
+
+        self.repository = repository
+
+
+    async def execute(self, vacancy_id: str, vacancy_status: StatusToUpdate) -> HttpResponse:
+
+        try:
+            found_vacancy: VacancyOutput | None = await self.repository.get_vacancy_by_id(vacancy_id)
+
+            if not found_vacancy:
+                return HttpResponseSchema.not_found(Exception(f"Vacancy not found for id: {vacancy_id}."))
+
+            updated_vacancy: VacancyOutput | None = await self.repository.edit_vacancy_status(vacancy_id, vacancy_status)
+
+            if not updated_vacancy:
+                return HttpResponseSchema.bad_request(Exception(f"Error while trying to update a vacancy for id: {vacancy_id}."))
+
+            update_vacancy_dict = json.loads(updated_vacancy.model_dump_json())
+            return HttpResponseSchema.ok(update_vacancy_dict)
+
+        except Exception as e:
+            traceback.print_exc()
+            return HttpResponseSchema.internal_server_error(Exception(e))
---
+++ tests/integration/api/v1/vacancy/test_patch.py
@@ -0,0 +1,54 @@
+import pytest
+import pytest_asyncio
+from httpx import AsyncClient
+import random
+from tests.integration.api.v1.vacancy.fake_data import generate_fake_vacancy_data
+
+@pytest_asyncio.fixture(loop_scope="class", autouse=True)
+async def setup_vacancy(request):
+    """
+        Runs once before all tests in the class.
+        Creates a test vacancy and stores it in the class.
+    """
+
+    http_client = AsyncClient(base_url="http://0.0.0.0:8000/api/v1")
+    request.cls.http_client = http_client
+
+    payload_input = generate_fake_vacancy_data()
+    response = await http_client.post("/vacancy/", json=payload_input)
+    assert response.status_code == 201
+
+    response_data = response.json()
+    request.cls.vacancy_id = response_data["payload"]["id"]
+
+    yield
+
+    await http_client.aclose()
+
+@pytest.mark.asyncio(loop_scope="class")
+class TestPatchVacancy:
+    http_client: AsyncClient = AsyncClient(base_url="http://0.0.0.0:8000/api/v1")
+
+    async def test_change_vacancy_status(self):
+
+        status_to_insert = random.choice(["FINISHED", "CANCELED"])
+
+        parameters = {
+            "vacancy_status": status_to_insert
+        }
+
+        response = await self.http_client.patch(
+            f"/vacancy/{self.vacancy_id}/status/",
+            params=parameters
+        )
+        assert response.status_code == 200
+
+        json_response: dict = response.json()
+
+        assert "status_code" in json_response
+        assert "payload" in json_response
+
+        payload = json_response["payload"]
+
+        assert payload["status"] == status_to_insert
+
