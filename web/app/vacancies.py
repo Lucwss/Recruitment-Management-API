b24628cf078deaf49bf7f01e27c3ab9dd06a0efc
lucas <lucws2002@gmail.com>
2025-07-27 23:48:06 -0400

    feat: Add route to fetch a vacancy by id and Add its dependency injection

--- web/app/vacancies.py
+++ web/app/vacancies.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter, Body, Depends
+from fastapi import APIRouter, Body, Depends, Path
 from fastapi.responses import JSONResponse
 from typing import Annotated
 
 from application.dto.vacancy import VacancyInput
 from domain.usecases.create_vacancy import CreateVacancyUseCase
-from web.dependencies import create_vacancy_use_case
+from domain.usecases.get_vacancy import GetVacancyUseCase
+from web.dependencies import create_vacancy_use_case, get_vacancy_use_case
 
 vacancy_router = APIRouter(
     prefix="/vacancy",
@@ -17,4 +18,12 @@ async def create_vacancy(
         use_case: Annotated[CreateVacancyUseCase, Depends(create_vacancy_use_case)]
 ):
     response = await use_case.execute(vacancy_data_input=vacancy_input)
+    return JSONResponse(content=response.model_dump(), status_code=response.status_code)
+
+@vacancy_router.get("/{vacancy_id}", summary='Route for getting a vacancy by ID.')
+async def get_vacancy(
+        vacancy_id: Annotated[str, Path(...)],
+        use_case: Annotated[GetVacancyUseCase, Depends(get_vacancy_use_case)]
+):
+    response = await use_case.execute(vacancy_id=vacancy_id)
     return JSONResponse(content=response.model_dump(), status_code=response.status_code)
--- web/dependencies.py
+++ web/dependencies.py
@@ -4,6 +4,7 @@ from fastapi import Depends
 
 from adapters.repositories.vacancy_repository import VacancyRepository
 from domain.usecases.create_vacancy import CreateVacancyUseCase
+from domain.usecases.get_vacancy import GetVacancyUseCase
 from infra.database.pgdatabase import Vacancy
 
 
@@ -22,3 +23,12 @@ def create_vacancy_use_case(
     """
 
     return CreateVacancyUseCase(repository)
+
+def get_vacancy_use_case(
+        repository: Annotated[VacancyRepository, Depends(vacancy_repository)],
+) -> GetVacancyUseCase:
+    """
+    function that injects the dependencies for GetVacancyUseCase
+    """
+
+    return GetVacancyUseCase(repository)
